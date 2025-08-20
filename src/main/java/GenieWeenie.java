939d927bc027b8a8e0f71e2a4b699cc02619c91d
nghaowei <e1397868@u.nus.edu>
2025-08-20 14:45:15 +0800

    Add, List

--- src/main/java/GenieWeenie.java
+++ src/main/java/GenieWeenie.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class GenieWeenie {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         String lineBorder = "____________________________________________________________";
+        String[] userText = new String[100];
+        int counter = 0;
+
         System.out.println(lineBorder);
         System.out.println(" Hello! I'm GenieWeenie");
         System.out.println(" What can I do for you?");
@@ -20,9 +25,21 @@ public class GenieWeenie {
                 break;
             }
 
-            System.out.println(lineBorder);
-            System.out.println(" " +input); // Echo
-            System.out.println(lineBorder);
+            else if (input.equalsIgnoreCase("list")) {
+                System.out.println(lineBorder);
+                for (int i = 0; i < counter; i++) {
+                    int listCount = i + 1;
+                    System.out.println(listCount + "." + userText[i]);
+                }
+                System.out.println(lineBorder);
+            }
+            else {
+                System.out.println(lineBorder);
+                System.out.println("added: " + input); // add words
+                System.out.println(lineBorder);
+                userText[counter] = input;
+                counter++;
+            }
         }
 
         sc.close();
