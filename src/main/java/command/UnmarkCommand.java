f650314a75dd61b75ed6beefeda0e5aa5300ba03
nghaowei <e1397868@u.nus.edu>
2025-09-19 23:36:37 +0800

    Update Storage to load and save tasks using List<Task> instead of array
    Modified Storage methods load() and save() to use List<Task> instead of arrays.
    
    Prevents array resizing issues and simplifies code.

--- src/main/java/command/UnmarkCommand.java
+++ src/main/java/command/UnmarkCommand.java
@@ -23,7 +23,7 @@ public class UnmarkCommand extends Command {
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws GenieweenieException {
         tasks.unmarkTask(index - 1);
-        response = "OK, I've marked this task as not done yet:\n "
+        response = "OK, I've marked this task as not done yet:\n  "
                 + tasks.getTask(index - 1);
         return response;
     }
